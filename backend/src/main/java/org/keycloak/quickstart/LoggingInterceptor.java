0cfd34a045320e490c45393812ec2767da1fcad9
Achmad Amri <achmad.amri@gmail.com>
2024-03-20 21:36:19 +0700

    Done for upload

--- backend/src/main/java/org/keycloak/quickstart/LoggingInterceptor.java
+++ backend/src/main/java/org/keycloak/quickstart/LoggingInterceptor.java
@@ -1,35 +1,51 @@
 package org.keycloak.quickstart;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("Request URL: {}", request.getRequestURL());
-        logger.info("Request Method: {}", request.getMethod());
-        logger.info("Request Parameters: {}", request.getParameterMap());
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        Map<String, Object> logMap = new HashMap<>();
+        logMap.put("Request URL", request.getRequestURL().toString());
+        logMap.put("Request Method", request.getMethod());
+        logMap.put("Request Parameters", request.getParameterMap());
+
+        try {
+            String logJson = objectMapper.writeValueAsString(logMap);
+            logger.info("Request: {}", logJson);
+        } catch (Exception e) {
+            logger.error("Error while logging request details", e);
+        }
+
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info("Response Status: {}", response.getStatus());
-    }
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        Map<String, Object> logMap = new HashMap<>();
+        logMap.put("Response Status", response.getStatus());
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // You can log more information here if needed
+        try {
+            String logJson = objectMapper.writeValueAsString(logMap);
+            logger.info("Response: {}", logJson);
+        } catch (Exception e) {
+            logger.error("Error while logging response details", e);
+        }
     }
 }
+
 
