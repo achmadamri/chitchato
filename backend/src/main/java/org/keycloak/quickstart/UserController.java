4fb2f08a2ba4411e8ea63c68c6eb8bd6a4fda4b3
Achmad Amri <achmad.amri@gmail.com>
2024-04-22 15:12:40 +0700

    Update cors

--- backend/src/main/java/org/keycloak/quickstart/UserController.java
+++ backend/src/main/java/org/keycloak/quickstart/UserController.java
@@ -35,6 +35,7 @@ import org.springframework.data.domain.Example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
